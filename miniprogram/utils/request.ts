ef8e40492dfa4059ab8d636b12180b694de6d4f3
jaye <jaye1248276@gmail.com>
2023-03-16 01:50:07 +0800

    [update]: 修改request配置和全局toast

--- miniprogram/utils/request.ts
+++ miniprogram/utils/request.ts
@@ -5,22 +5,30 @@ const simpleAxios = SimpleAxios.create({
   baseURL: 'https://netease-music-api.fe-mm.com'
 })
 
-simpleAxios.interceptors.request.use((config) => {
-  console.log('%c🚀 ~ method: request ~', 'color: #F25F5C;font-weight: bold;', config)
-  toast.start()
-  return config
-})
-
-simpleAxios.interceptors.response.use((response) => {
-  const { data, statusCode, errMsg } = response
-  console.log('%c🚀 ~ method: response ~', 'color: #F25F5C;font-weight: bold;', response)
-  if (statusCode !== 200 || data?.code !== 200) {
+simpleAxios.interceptors.request.use(
+  (config) => {
+    console.log('%c🚀 ~ method: request ~', 'color: #F25F5C;font-weight: bold;', config)
+    toast.start()
+    return config
+  }, (err) => {
     toast.fail()
-    return Promise.reject(new Error(errMsg))
-  }
+    return Promise.reject(err)
+  })
 
-  toast.close()
-  return response
-})
+simpleAxios.interceptors.response.use(
+  (response) => {
+    const { data, statusCode, errMsg } = response
+    console.log('%c🚀 ~ method: response ~', 'color: #F25F5C;font-weight: bold;', response)
+    if (statusCode !== 200 || (data as Record<string, any>)?.code !== 200) {
+      toast.fail()
+      return Promise.reject(new Error(errMsg))
+    }
+
+    toast.close()
+    return data
+  }, (err) => {
+    toast.fail()
+    return Promise.reject(err)
+  })
 
 export default simpleAxios
