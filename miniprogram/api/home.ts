54187adf484f83bc32f1fd9667636a94c69ed760
jaye <jaye1248276@gmail.com>
2023-03-16 01:52:57 +0800

    [feat]: Home子页swiper完成

---
+++ miniprogram/api/home.ts
@@ -0,0 +1,16 @@
+import simpleAxios from '../utils/request'
+
+export interface BannerResponse{
+  banners: {
+    bannerBizType: string
+    imageUrl: string
+    targetId: number
+    targetType: number
+    titleColor: string
+    typeTitle: string
+  }[]
+  code: number
+}
+export function getBanner() {
+  return simpleAxios.get<BannerResponse>('/banner')
+}
