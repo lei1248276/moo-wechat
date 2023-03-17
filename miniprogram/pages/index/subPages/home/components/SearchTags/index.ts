82f4738b8020e99704198b65ed6668decbdb75a1
jaye <jaye1248276@gmail.com>
2023-03-17 21:57:08 +0800

    [feat]: Home页面SearchTags组件完成

---
+++ miniprogram/pages/index/subPages/home/components/SearchTags/index.ts
@@ -0,0 +1,15 @@
+Component({
+  options: {
+    styleIsolation: 'shared'
+  },
+  properties: {
+
+  },
+  data: {
+    tags: ['#流行音乐奖', '#Contemporary R&B', '#朋友圈', '#新城劲爆颁奖礼',
+      '#千禧曼波', '#新城国语力颁奖礼', '#太能说了', '#New Wave', '#活下去']
+  },
+  methods: {
+
+  }
+})
