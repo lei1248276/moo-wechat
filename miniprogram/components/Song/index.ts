b8791ac1301254eb8574f627cd558b5645aebab2
jaye <jaye1248276@gmail.com>
2023-04-02 16:29:04 +0800

    [refactor]: 抽离Playlist组件结构，新增Song组件

---
+++ miniprogram/components/Song/index.ts
@@ -0,0 +1,24 @@
+Component({
+  options: {
+    styleIsolation: 'shared'
+  },
+  properties: {
+    song: {
+      type: Object
+    },
+    isPlay: {
+      type: Boolean,
+      value: false
+    },
+    isRun: {
+      type: Boolean,
+      value: false
+    }
+  },
+  data: {
+
+  },
+  methods: {
+
+  }
+})
